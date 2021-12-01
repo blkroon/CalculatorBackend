2bbc27d2c27f921a85fd3a02011fdfdd49f579ad
Bram Kroon <bkroon@bkroon-pc.quintor.local>
2021-12-01 09:38:08 +0100

    controller endpoints

--- src/main/java/nl/quintor/calculator/controller/CalculatorController.java
+++ src/main/java/nl/quintor/calculator/controller/CalculatorController.java
@@ -1,7 +1,11 @@
 package nl.quintor.calculator.controller;
 
 import lombok.AllArgsConstructor;
+import nl.quintor.calculator.controller.dto.ValuesDTO;
 import nl.quintor.calculator.service.SimpleCalculator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +16,27 @@ public class CalculatorController {
 
     private SimpleCalculator simpleCalculator;
 
+    @PostMapping("/add")
+    public ResponseEntity<Double> add(@RequestBody ValuesDTO values) {
+        double result = simpleCalculator.add(values.getValue1(), values.getValue2());
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/subtract")
+    public ResponseEntity<Double> subtract(@RequestBody ValuesDTO values) {
+        double result = simpleCalculator.subtract(values.getValue1(), values.getValue2());
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/multiply")
+    public ResponseEntity<Double> multiply(@RequestBody ValuesDTO values) {
+        double result = simpleCalculator.multiply(values.getValue1(), values.getValue2());
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/divide")
+    public ResponseEntity<Double> divide(@RequestBody ValuesDTO values) {
+        double result = simpleCalculator.divide(values.getValue1(), values.getValue2());
+        return ResponseEntity.ok(result);
+    }
 }
